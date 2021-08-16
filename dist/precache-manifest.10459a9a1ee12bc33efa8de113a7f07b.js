a1794ed4656721e52f709848acf9f99802c4dc6f
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-08-17 01:00:20 +0800

    updates

---
+++ dist/precache-manifest.10459a9a1ee12bc33efa8de113a7f07b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4555d34127479b15fa88",
+    "url": "/css/app.bceea02a.css"
+  },
+  {
+    "revision": "89cc4e33ab204a56668520b6d0511800",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "d44572f537210803b78a4d3303931008",
+    "url": "/img/profile-img.d44572f5.jpg"
+  },
+  {
+    "revision": "6bc0ff1f01e9bdb5081778fae120c7d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4555d34127479b15fa88",
+    "url": "/js/app.a686953d.js"
+  },
+  {
+    "revision": "475c12a2677770aa105a",
+    "url": "/js/chunk-vendors.2cc13e65.js"
+  },
+  {
+    "revision": "a5f6e57f48c2072b5794121701c8bdbb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
